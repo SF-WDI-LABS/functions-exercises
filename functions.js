2f7b85c29000e80603d336cacde31a1fad9f335c
jimhantrix <jimtekanyo@gmail.com>
2016-03-16 10:56:57 -0700

    homework

--- functions.js
+++ functions.js
@@ -1,1 +1,102 @@
-// TODO: add your solutions here!
+
+//1.build concentation  (return)
+function combineWords(word1, word2) {
+
+    var result === word1+word2;
+    console.log(result);
+    return result;
+}
+combineWords ("dog","house")
+
+
+//2.repeat phrase  (display)
+function repeatPhrase(phrase, n){
+    for (var i===0; i< n ; i++){
+    console.log(phrase, n);
+}
+
+repeatPhrase("Hello", 5);
+
+
+//3.power function  (return)
+function toTheNthPower(number,power){
+    var result = 1
+    for (var i = 0; i < power ,i ++);
+}
+
+to the NthPower(4, 5);
+return results ;1024
+
+
+
+//4. Area of a circle  (return)
+function areaOfACircle(radius){
+ var i ===3.14
+  var result ( i * radius * 2 )
+}
+areaOfACircle(2);
+return  result; 12.57
+
+
+//5. Pythagorean theorema(return)
+function  pythagoreanTheorem( a, b){
+var result( a + b)*2)===c);
+}
+pythagoreanTheorem (3,4);
+return ; true
+
+
+
+//6. x divisible
+function isXEvenlyDivisibleByY(x,y){
+   var result  x % y = 0;
+   console.log(result)
+}
+  isXEvenlyDivisibleByY(99,33);
+   return result ;true
+
+//7. vowel count  (return)
+
+function countVowels(word) {
+  var count =0
+  word = word.toLowerCase();
+  for var i = 0 ; i < word.length ; i++{
+    if word === Vowels
+    return count
+  }
+
+}
+ countVowels("stealing")
+ return 3
+
+
+ //8 Triangle
+
+ unction printTriangle(length) {
+ 	for(var i = 1; i <= length; i++) {
+
+ 		var triangle = '';
+
+ 		for(var j = 0; j < i; j++) {
+ 			triangle += '*';
+ 		}
+
+ 		console.log(triangle);
+ 	}
+ }
+
+ //9
+ function printPyramid(length) {
+ 	for(var i = 1; i <= length; i++) {
+ 		var tier = '';
+
+ 		for(var j = length - i; j > 0; j--) {
+ 			tier += ' ';
+ 		}
+ 		for(var k = 0; k < i; k++) {
+ 			tier += '* ';
+ 		}
+ 		console.log(tier);
+ 	}
+ }
+ return (10);
