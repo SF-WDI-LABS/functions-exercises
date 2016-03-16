186848515a912596ed597f6dbaf880269f904a64
mbd-s <michaelbsilverberg@gmail.com>
2016-03-15 18:05:39 -0700

    completed through #8

--- functions.js
+++ functions.js
@@ -46,4 +46,53 @@ function pythagoreanTheorem(a, b) {
 }
 
 pythagoreanTheorem(3, 4);
-// should display 5;
+
+
+//6 Return a boolean value whether or not X can be divided by Y without any remainders.
+
+function isXEvenlyDivisibleByY(x, y) {
+    if (x % y === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+isXEvenlyDivisibleByY(100, 3);
+
+
+//7 Return the number of occurences of vowels in a word. Vowels are a, e, i, o, u, and y
+//Challenge: Can you alter the code to count both upper case AND lower case?
+
+function countVowels(word) {
+  var vowelCount = 0;
+
+    for (i = 0; i < word.length; i++) {
+
+      if  (word.charAt(i) === "a" || word.charAt(i) === "e" ||  word.charAt(i) === "i" || word.charAt(i) === "o" || word.charAt(i) === "u" || word.charAt(i) === "y") {
+        vowelCount++;
+        }
+      }
+      return vowelCount;
+}
+
+countVowels("stealing");
+
+
+//8 Display a simple triangle with asterisks
+
+function printTriangle(length) {
+  var space = ' ';
+  for (i=0; i<length; i++) {
+
+    space += "*";
+    console.log(space);
+  }
+}
+
+printTriangle(3);
+// displays
+// *
+// **
+// ***
