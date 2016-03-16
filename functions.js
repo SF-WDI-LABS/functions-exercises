989ecc8d90a3ad953d9b7b03657cfc417a98a659
cloverharvest <anna@schwabdev.com>
2016-03-15 17:29:06 -0700

    Pushing answers for 1 to 4 of functions exercise

--- functions.js
+++ functions.js
@@ -1,1 +1,56 @@
+//Notes to self: Use return instead of console.log within the function as necessary
+//Call the function when submitting this
+
 // TODO: add your solutions here!
+
+//1.
+
+function combineWords(word1, word2) {
+  return word1 + word2;
+}
+
+combineWords('dog', 'house');
+
+//OR
+
+function combineWords(word1, word2) {
+  return word1.concat(word2);
+}
+
+combineWords('dog', 'house');
+
+//2.
+
+function repeatPhrase(phrase, n) {
+	for (i = 0; i < n; i++) {
+	    console.log(phrase);
+	}
+}
+
+repeatPhrase("Hello", 5);
+console.log();
+
+//3.
+
+var result = 1;
+
+function toTheNthPower(number, power) {
+  for (var i = 0; i < power; i++) {
+    result *= number;
+  }
+  return(result);
+}
+
+toTheNthPower(4, 5);
+
+//4.
+
+function areaOfACircle(radius) {
+  var rsquared = radius * radius;
+  console.log(rsquared);
+	var area = Math.PI * rsquared;
+	console.log(area);
+  return area;
+}
+
+areaOfACircle(2);
