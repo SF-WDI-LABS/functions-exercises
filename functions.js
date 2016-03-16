e5684d50324f02ab9be8cbbeb1e0d260694069d9
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-03-16 00:44:20 -0700

    solution attempts

--- functions.js
+++ functions.js
@@ -1,1 +1,97 @@
-// TODO: add your solutions here!
+// 1
+
+function combineWords(word1, word2) {
+  console.log(word1 + word2);
+
+}
+
+combineWords('dog', 'house');
+
+// 2
+
+function repeatPhrase(phrase, n) {
+  for (var i = 0; i < n; i++) {
+    console.log(phrase);
+  }
+
+}
+
+repeatPhrase("Hello", 5);
+
+//3
+
+function toTheNthPower(number, power) {
+  var result = 1;
+  for (var i = 0; i < power; i++) {
+    result *= number;
+  }
+
+  console.log(result);
+  return result;
+}
+
+
+toTheNthPower(4, 5);
+
+//4
+
+function areaOfACircle(radius) {
+  var r = radius * radius;
+  var timesPi = Math.PI * r;
+  console.log(timesPi);
+
+}
+
+areaOfACircle(2);
+
+//5
+
+function pythagoreanTheorem(a, b) {
+  var a = (a * a);
+  var b = (b * b);
+  return Math.sqrt(a + b);
+}
+
+
+pythagoreanTheorem(3, 4);
+
+//6
+
+function isXEvenlyDivisibleByY(x, y) {
+  if (x % y === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isXEvenlyDivisibleByY(99, 3);
+
+//7
+
+function countVowels(word) {
+  var theVowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  for (var i = 0; i < countVowels.length; i++) {
+    if (theVowels['0'], ['1'], ['2'], ['3'], ['4'], ['5']); {
+      return countVowels.length;
+    }
+
+  }
+}
+}
+
+countVowels("stealing");
+
+// HELP here, feel close!!
+
+//8
+
+function printTriangle(length) {
+  var star = "*";
+  if (star true); {
+    console.log("*")
+  }
+
+}
+
+printTriangle(3);
