3fb6dbcb0da722488c91f402abcbef3aa7e6dbd8
lsquach <lsquach@gmail.com>
2016-03-15 21:35:22 -0700

    Uploading solutions to functions exercises

--- functions.js
+++ functions.js
@@ -1,1 +1,66 @@
-// TODO: add your solutions here!
+//1.
+function combineWords(word1, word2) {
+    return word1 + word2;
+}
+
+combineWords('dog', 'house');
+
+//2
+function repeatPhrase(phrase, n) {
+    for(var i = 0; i < n; i++) {
+        console.log(phrase);
+    }
+}
+
+repeatPhrase("Hello", 5);
+
+//3
+function toTheNthPower(number, power) {
+    var result = 1;
+    for(var i = 0; i < power; i++) {
+        result = result * number;
+    }
+    return result;
+}
+
+toTheNthPower(4, 5);
+
+//4
+function areaOfACircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+areaOfACircle(2);
+
+//5
+function pythagoreanTheorem(a, b) {
+    var x = (a * a) + (b * b);
+    return Math.sqrt(x);
+}
+
+//6
+function isXEvenlyDivisibleByY(x, y) {
+    var remainder = x % y;
+    if (remainder === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+isXEvenlyDivisibleByY(99, 3);
+
+//7
+function countVowels(word) {
+	var result = 0;
+	word = word.toLowerCase();
+	for(var i = 0; i < word.length; i++) {
+		if( word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' ||  word[i] === 'u' ||  word[i] === 'y'){
+			result++;
+		}
+	}
+	return result;
+}
+
+countVowels("stealing");
