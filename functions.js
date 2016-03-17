21648b3e69cc2533fd861ab436489739aaf6ded4
jimhantrix <jimtekanyo@gmail.com>
2016-03-16 23:42:42 -0700

    make changes on the f.js

--- functions.js
+++ functions.js
@@ -1,102 +1,50 @@
 
-//1.build concentation  (return)
-function combineWords(word1, word2) {
 
-    var result === word1+word2;
-    console.log(result);
-    return result;
-}
-combineWords ("dog","house")
+// sillySum solution
 
-
-//2.repeat phrase  (display)
-function repeatPhrase(phrase, n){
-    for (var i===0; i< n ; i++){
-    console.log(phrase, n);
+var sillySum = function(arr) {
+    var total = 0;
+    for (var i = 0; i < arr.length; i += 1) {
+    total + = arr[i] * i;
 }
 
-repeatPhrase("Hello", 5);
+return total;
 
 
-//3.power function  (return)
-function toTheNthPower(number,power){
-    var result = 1
-    for (var i = 0; i < power ,i ++);
+
+  // isPrime solution
+
+  var isPrime = function(num) {
+      if (num < 2) {
+      return false;
+}
+      for (var i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+      return false;
 }
 
-to the NthPower(4, 5);
-return results ;1024
+return true;
 
 
 
-//4. Area of a circle  (return)
-function areaOfACircle(radius){
- var i ===3.14
-  var result ( i * radius * 2 )
-}
-areaOfACircle(2);
-return  result; 12.57
 
+  //filter
 
-//5. Pythagorean theorema(return)
-function  pythagoreanTheorem( a, b){
-var result( a + b)*2)===c);
-}
-pythagoreanTheorem (3,4);
-return ; true
+var filter = function(arr, callback) {
 
+      var newArr = i;
 
+      for(var i = 0; i < arr.length; i++){
 
-//6. x divisible
-function isXEvenlyDivisibleByY(x,y){
-   var result  x % y = 0;
-   console.log(result)
-}
-  isXEvenlyDivisibleByY(99,33);
-   return result ;true
-
-//7. vowel count  (return)
-
-function countVowels(word) {
-  var count =0
-  word = word.toLowerCase();
-  for var i = 0 ; i < word.length ; i++{
-    if word === Vowels
-    return count
-  }
-
-}
- countVowels("stealing")
- return 3
-
-
- //8 Triangle
-
- unction printTriangle(length) {
- 	for(var i = 1; i <= length; i++) {
-
- 		var triangle = '';
-
- 		for(var j = 0; j < i; j++) {
- 			triangle += '*';
- 		}
-
- 		console.log(triangle);
- 	}
+      if (callback(arr[i]))
  }
 
- //9
- function printPyramid(length) {
- 	for(var i = 1; i <= length; i++) {
- 		var tier = '';
+      newArr.push(arr[i]);{
 
- 		for(var j = length - i; j > 0; j--) {
- 			tier += ' ';
- 		}
- 		for(var k = 0; k < i; k++) {
- 			tier += '* ';
- 		}
- 		console.log(tier);
- 	}
- }
- return (10);
+      return newArr;
+
+      for var isOdd = function(num) {     
+      return num % 2 !== 0;
+
+      if else (var newArr > arr.length;i -=0)
+      return false;
